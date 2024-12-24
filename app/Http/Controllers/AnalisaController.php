77661240f1f872e3656a358e784008fcd0041eb3
Ramandhika <ramandhika.ilham@gmail.com>
2024-12-24 21:26:04 +0700

    Refactor AnalisaController to enhance analysis functionality; implement weighted moving average calculations and prediction for January sales

--- app/Http/Controllers/AnalisaController.php
+++ app/Http/Controllers/AnalisaController.php
@@ -19,48 +19,108 @@ class AnalisaController extends Controller
 
     public function analisis(Request $request)
     {
-        $dataPenjualan = Penjualan::whereBetween('tahun', [$request->tahunAwal, $request->tahunAkhir])
-            ->whereBetween('bulan', [$request->bulanAwal, $request->bulanAkhir])
-            ->get();
+        $months = [
+            'Januari' => 1, 'Februari' => 2, 'Maret' => 3, 'April' => 4,
+            'Mei' => 5, 'Juni' => 6, 'Juli' => 7, 'Agustus' => 8,
+            'September' => 9, 'Oktober' => 10, 'November' => 11, 'Desember' => 12
+        ];
 
-        $wma = [];
-        $mad = [];
-        $mse = [];
-        $mape = [];
-        $weights = [0.70, 0.20, 0.10];
-        $totalWeight = array_sum($weights);
+        $bulanAwal = $months[$request->bulanAwal];
+        $bulanAkhir = $months[$request->bulanAkhir];
 
-        foreach ($dataPenjualan as $index => $penjualan) {
-            if ($index >= 3) {
-                $wmaValue = (
-                    $dataPenjualan[$index - 1]->penjualan * $weights[0] +
-                    $dataPenjualan[$index - 2]->penjualan * $weights[1] +
-                    $dataPenjualan[$index - 3]->penjualan * $weights[2]
-                ) / $totalWeight;
+        $penjualan = Penjualan::where(function($query) use ($request, $bulanAwal) {
+            $query->where('tahun', '>', $request->tahunAwal)
+                  ->orWhere(function($query) use ($request, $bulanAwal) {
+                      $query->where('tahun', '=', $request->tahunAwal)
+                            ->where('bulan', '>=', $bulanAwal);
+                  });
+        })->where(function($query) use ($request, $bulanAkhir) {
+            $query->where('tahun', '<', $request->tahunAkhir)
+                  ->orWhere(function($query) use ($request, $bulanAkhir) {
+                      $query->where('tahun', '=', $request->tahunAkhir)
+                            ->where('bulan', '<=', $bulanAkhir);
+                  });
+        })->orderBy('tahun')->orderBy('bulan')->get();
 
-                $wma[] = $wmaValue;
-                $mad[] = abs($penjualan->penjualan - $wmaValue);
-                $mse[] = pow($mad[count($mad) - 1], 2);
-                $mape[] = $mad[count($mad) - 1] / $penjualan->penjualan;
+        // return response()->json([
+        //     'penjualan' => $penjualan,
+        //     'request' => $request->all()
+        // ]);
+        $weights = [0.1, 0.2, 0.7]; // Bobot
+
+        $result = [];
+        $totalMAD = 0;
+        $totalMSE = 0;
+        $totalMAPE = 0;
+
+        foreach ($penjualan as $key => $data) {
+            if ($key >= 3) { // Mulai dari bulan ke-4
+                $wma = (
+                    ($penjualan[$key - 1]->jumlah * $weights[2]) +
+                    ($penjualan[$key - 2]->jumlah * $weights[1]) +
+                    ($penjualan[$key - 3]->jumlah * $weights[0])
+                );
+
+                $mad = abs($data->jumlah - $wma);
+                $mse = pow($mad, 2);
+                $mape = $mad / $data->jumlah;
+
+                $totalMAD += $mad;
+                $totalMSE += $mse;
+                $totalMAPE += $mape;
+
+                $result[] = [
+                    'tahun' => $data->tahun,
+                    'bulan' => $data->bulan,
+                    'index' => $key + 1,
+                    'penjualan' => $data->jumlah,
+                    'wma' => $wma,
+                    'mad' => $mad,
+                    'mse' => $mse,
+                    'mape' => $mape
+                ];
+            } else {
+                $result[] = [
+                    'tahun' => $data->tahun,
+                    'bulan' => $data->bulan,
+                    'index' => $key + 1,
+                    'penjualan' => $data->jumlah,
+                    'wma' => null,
+                    'mad' => null,
+                    'mse' => null,
+                    'mape' => null
+                ];
             }
         }
 
-        $totalMad = array_sum($mad);
-        $totalMse = array_sum($mse);
-        $totalMape = array_sum($mape);
+        $averageMAD = $totalMAD / count($result);
+        $averageMSE = $totalMSE / count($result);
+        $averageMAPE = $totalMAPE / count($result);
+        // Prediksi bulan Januari berikutnya
+        $lastData = end($penjualan);
+        $prediksiJanuari = (
+            ($penjualan[count($penjualan) - 1]->jumlah * $weights[2]) +
+            ($penjualan[count($penjualan) - 2]->jumlah * $weights[1]) +
+            ($penjualan[count($penjualan) - 3]->jumlah * $weights[0])
+        );
 
-        $averageMad = $totalMad / count($mad);
-        $averageMse = $totalMse / count($mse);
-        $averageMape = ($totalMape / count($mape)) * 100;
-        return json_decode(json_encode([
-            'wma' => $wma,
-            'mad' => $mad,
-            'mse' => $mse,
-            'mape' => $mape,
-            'averageMad' => $averageMad,
-            'averageMse' => $averageMse,
-            'averageMape' => $averageMape,
-        ]));
-        return view('analisa.result', compact('wma', 'mad', 'mse', 'mape', 'averageMad', 'averageMse', 'averageMape'));
+        $result[] = [
+            'tahun' => $penjualan[count($penjualan) - 1]->tahun + 1,
+            'bulan' => 'Januari',
+            'index' => count($penjualan) + 1,
+            'penjualan' => null,
+            'wma' => $prediksiJanuari,
+            'mad' => null,
+            'mse' => null,
+            'mape' => null
+        ];
+
+        return response()->json([
+            'result' => $result,
+            'averageMAD' => $averageMAD,
+            'averageMSE' => $averageMSE,
+            'averageMAPE' => $averageMAPE,
+            'prediksiJanuari' => $prediksiJanuari
+        ]);
     }
 }
